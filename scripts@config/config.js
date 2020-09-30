539e0ef6e16a752e7909c63dbb3aa49b8fe2fe86
cloudres <pluralcrocodiles@gmail.com>
2020-09-30 12:21:58 +0800

    :green_heart:rewrite config module to global variable

--- scripts@config/config.js
+++ scripts@config/config.js
@@ -1,59 +1,82 @@
-// inner
-const fadeOutSpeedLevel = 4;
+{
+  // inner
+  const fadeOutSpeedLevel = 4;
 
-// export
-const TestMode = true,
+  // export
+  const UseNewBasejs = true,
+  
+  TestMode = true,
 
-themeColor = '#000', // used in , webmanifest
+  themeColor = '#000', // used in , webmanifest
 
-BackgroundColor = '#f7d9aa', // used in index.html/meta-themeColor, style-background
+  BackgroundColor = '#f7d9aa', // used in index.html/meta-themeColor, style-background
 
 
-Version = '1.2.0' + '--dev', //NOTE: 添加功能后记得更改这个
+  Version = '1.2.0' + '--dev', //NOTE: 添加功能后记得更改这个
 
-PerspectiveCameraSetting = {
-    fieldOfView: 60,
-    aspectRatio: window.innerWidth / window.innerHeight,
-    nearPlane: 1,
-    farPlane: 10000
-},
+  PerspectiveCameraSetting = {
+      fieldOfView: 60,
+      aspectRatio: window.innerWidth / window.innerHeight,
+      nearPlane: 1,
+      farPlane: 10000
+  },
 
-GetContainer = () => {
-  return document.getElementById('canvas')
-},
+  GetContainer = () => {
+    return document.getElementById('canvas')
+  },
 
-GameStartCallback = async () => { // asynchronous 异步的，非同时的
-  const loadSection = document.getElementById('loading-section');
-  const loader = document.getElementById('loader');
+  GameStartCallback = async () => { // asynchronous 异步的，非同时的
+    /**
+     * 渐隐目标对象
+     * @param {*} currentOpacity 初始透明度
+     * @param {*} reduction_num 每次执行改变的幅度
+     * @param {*} per_ms 每隔多少毫秒执行一次。单位：毫秒
+     * @param {*} reference 指向的对象
+     */
+    const fadeOut = (currentOpacity, reduction_num, per_ms, reference) => { 
+      (reference.style.opacity = currentOpacity) < .1
+      ? (reference.hidden = true) && (reference.style.willChange = 'auto')
+      : setTimeout(() => fadeOut(currentOpacity - reduction_num, reduction_num, per_ms, reference), per_ms)
+    }
 
-  const reduction_num1 = .02 * fadeOutSpeedLevel,
-        reduction_num2 = .03 * fadeOutSpeedLevel;
+    const loadSection = document.getElementById('loading-section');
+    const loader = document.getElementById('loader');
 
-  const fadeOut = (num1, num2, speed) => {
-    (loadSection.style.opacity = num1) <= .1
-        ? (loadSection.hidden = true) && (loadSection.style.willChange = loader.style.willChange = 'auto')
-        : (loader.style.opacity = num2) 
-          && setTimeout(()=>fadeOut(num1 - reduction_num1, num2 - reduction_num2, speed), speed) // speed单位：毫秒
+    setTimeout(()=>{
+      if(loadSection.hidden !== true){
+        loadSection.style.willChange = "opacity";
+        fadeOut(1, .02 * fadeOutSpeedLevel, 25 * fadeOutSpeedLevel, loadSection);
+      }
+      if(loader.hidden !== true){
+        loader.style.willChange = "opacity";
+        fadeOut(1, .03 * fadeOutSpeedLevel, 25 * fadeOutSpeedLevel, loader);
+      }
+    }, config?.loading_timeOut ?? 400)
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Nullish_Coalescing_Operator
+    // fallback: window.config && config.loading_timeOut ? config.loading_timeOut : 400
+  },
+
+  RotationSpeed_Sea = .005,
+  RotationSpeed_Sky = .01
+
+  ;
+
+  // export default {
+  window.config = {
+      useNewBasejs: UseNewBasejs,
+      testMode: TestMode,
+      version: Version,
+      cameraSetting: PerspectiveCameraSetting,
+      getContainer: GetContainer,
+      gameStartCallback: GameStartCallback,
+      speed_sea: RotationSpeed_Sea,
+      speed_sky: RotationSpeed_Sky
   }
-  setTimeout(()=>{
-    loadSection.style.willChange = "opacity";
-    loader.style.willChange = "opacity";
-    fadeOut(1, 1, 25 * fadeOutSpeedLevel);
-  }, 400)
-},
-
-RotationSpeed_Sea = .005,
-RotationSpeed_Sky = .01
-
-;
-// ...
-
-export default {
-    testMode: TestMode,
-    version: Version,
-    cameraSetting: PerspectiveCameraSetting,
-    getContainer: GetContainer,
-    gameStartCallback: GameStartCallback,
-    speed_sea: RotationSpeed_Sea,
-    speed_sky: RotationSpeed_Sky
 }
+
+window.throwError = function(...args) {
+  document.getElementById('show-error').removeAttribute('hidden')
+  document.getElementById('errorLog').innerText = args.join('\n');
+  console.error.apply(this, arguments);
+};
