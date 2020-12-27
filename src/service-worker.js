b09b79203fa255f0c1f90a8850e332db88231a3f
edfus <edfusedfus@gmail.com>
2020-12-27 16:46:40 +0800

    Introducing better compatibility with babel

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,5 +1,6 @@
-const mode = ["network-first", "offline-first"][0];
-const version = "8.2.6";
+import fetchHandler from "./service-worker/network-first";
+
+const version = "1.1.3";
 const cacheName = "cache-" + version;
 const cacheResources = [
   `/`, 
@@ -38,65 +39,4 @@ self.addEventListener('activate', e => {
   return self.clients.claim();
 });
 
-
-if(mode === "network-first") {
-  self.addEventListener('fetch', e => 
-    e.respondWith(
-      (async () => {
-        let response = await e.preloadResponse;
-
-        if (!response) {
-          try {
-            response = await fetch(e.request)
-          } finally {
-            if(!response) {
-              const cacheResponse = await caches.match(e.request.url)
-              return cacheResponse ? cacheResponse : Response.error();
-            } else if (response.status !== 200) {
-              if(response.status === 0)
-                ; // no-cors opaque response
-              else {
-                const cacheResponse = await caches.match(e.request.url)
-                return cacheResponse ? cacheResponse : response;
-              }
-            }
-          }
-        }
-
-        // got non-cached ok response
-        if(e.request.method === "GET" && ["opaque", "cors", "basic"].includes(response.type) || /(\.mp3)$/.test(request.url)) {
-          const clone = response.clone();
-          e.waitUntil(
-            caches.open(cacheName).then(cache => cache.put(e.request.url, clone))
-          )
-          // put() will overwrite any key/value pair previously stored in the cache that matches the request.
-        }
-        return response;
-      })()
-    )
-  )
-} else {
-  self.addEventListener('fetch', e => 
-    e.respondWith(
-      caches.match(e.request.url).then(async response => {
-        if (response) {
-          return response;
-        } else {
-          const request = e.request.clone();
-          const response = await fetch(request);
-
-          if (response.status !== 200 || !["cors", "basic"].includes(response.type) || /(\.mp3)$/.test(request.url)) {
-            return response;
-          }
-          if(request.method === "GET") {
-            const clone = response.clone();
-            e.waitUntil(
-              caches.open(cacheName).then(cache => cache.put(request.url, clone))
-            )
-          }
-          return response;
-        }
-      })
-    )
-  )
-}
+self.addEventListener('fetch', fetchHandler);
