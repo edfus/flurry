edf9c6e34ad30678abf3fda42f6a11baeeab9330
edfus <edfusedfus@gmail.com>
2020-12-27 15:00:40 +0800

    Fix some errors.

--- rollup/builder.js
+++ rollup/builder.js
@@ -6,14 +6,21 @@ import jsCompiler from './compilers/rollup.js';
 import jsMinifier from "./compilers/terser.js";
 import stylusCompiler from './compilers/stylus.js';
 
+
 const offset = join(__dirname, "../");
 
 let config = {
   js: {
+      match: {
+        test (fileName) {
+          return !/(service-worker.js)$/.test(fileName);
+        }
+      },
       src: offset.concat("src/main.js"),
-      dest: offset.concat("dist"),
+      dest: offset.concat("dist")
   },
   css: {
+      match: /.*/,
       src: offset.concat("src/styles/style.styl"),
       dest: offset.concat("dist"),
   }
@@ -22,12 +29,15 @@ let config = {
 /**
  * rollup, babel, minify.
  */
-async function buildJS() {
-  const file = resolve(config.js.src);
+async function buildJS(file, dest) {
+  if(config.js.match.test(file))
+    file = config.js.src;
+  else if(!dest)
+    throw new Error(`buildJS: ${file} !match && !dest`);
 
   const outputFile = await jsCompiler({
-      src:  file,
-      dest: resolve(config.js.dest),
+      src: resolve(file),
+      dest: resolve(dest || config.js.dest),
       fileName: `{name}@${version}.js`,
       format: 'umd', // amd, cjs, es, iife, umd, system. esm? universal module.
       sourcemap: false,
@@ -36,7 +46,7 @@ async function buildJS() {
 
   await jsMinifier({
       file: outputFile,
-      path_o: resolve(config.js.dest),
+      path_o: resolve(dest || config.default_dest),
       fileName: `{name}.min.js`
   })
 
@@ -47,11 +57,14 @@ async function buildJS() {
 /**
 * Handle Postcss files
 */
-async function buildCSS() {
-  const file = resolve(config.css.src);
+async function buildCSS(file) {
+  if(config.css.match.test(file))
+    file = config.css.src;
+  else if(!dest)
+    throw new Error(`buildCSS: ${file} !match && !dest`);
 
   await stylusCompiler({
-        file,
+        file: resolve(file),
         dest: resolve(config.css.dest),
         fileName: `{name}@${version}.min.css`,
         minify: true,
@@ -65,8 +78,9 @@ async function build () {
   return (
       Promise.all(
           [
-              buildJS(),
-              buildCSS()
+              buildJS(config.js.src),
+              buildJS(offset.concat("src/service-worker.js"), offset.concat("www")),
+              buildCSS(config.css.src)
           ]
       )
   );
